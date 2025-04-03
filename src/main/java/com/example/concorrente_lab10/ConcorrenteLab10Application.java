92075f0b1e9b6c51f926a4d8c491f163a27039a9
Arthurabriel <arthur-gabriel-santos-albuquerque@ccc.ufcg.edu.br>
2025-04-03 16:36:29 -0300

    Create Project

---
+++ src/main/java/com/example/concorrente_lab10/ConcorrenteLab10Application.java
@@ -0,0 +1,13 @@
+package com.example.concorrente_lab10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcorrenteLab10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcorrenteLab10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concorrente_lab10/ConcorrenteLab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concorrente_lab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcorrenteLab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
